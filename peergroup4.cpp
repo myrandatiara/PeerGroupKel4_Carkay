188406162d8d921d7453ed0894e233e87a09d4d7
myrandatiara <myrandaokke@gmail.com>
2025-10-11 09:53:37 +0700

    peergroup4.cpp

--- peergroup4.cpp
+++ peergroup4.cpp
@@ -1,53 +1,58 @@
 #include <iostream>
 using namespace std;
 
+double tambah(double a, double b) { return a + b; }
+double kurang(double a, double b) { return a - b; }
+double kali(double a, double b) { return a * b; }
+double bagi(double a, double b) {
+    if (b == 0) {
+        cout << "Error: Tidak bisa dibagi dengan nol!" << endl;
+        return 0;
+    }
+    return a / b;
+}
+
 int main() {
     int pilihan;
     double a, b, hasil;
+    char ulang;
 
-    cout << "== Kalkulator Sederhana ==" << endl;
-    cout << "1. Penjumlahan" << endl;
-    cout << "2. Pengurangan" << endl;
-    cout << "3. Perkalian" << endl;
-    cout << "4. Pembagian" << endl;
-    cout << "Masukkan pilihan (1-4): ";
-    cin >> pilihan;
+    do {
+        cout << "\n== Kalkulator Sederhana ==" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "Masukkan pilihan (1-4): ";
+        cin >> pilihan;
 
-    if (pilihan == 1) {
         cout << "Masukkan angka pertama: ";
         cin >> a;
         cout << "Masukkan angka kedua: ";
         cin >> b;
-        hasil = a + b;
-        cout << "Hasil: " << hasil << endl;
-    } else if (pilihan == 2) {
-        cout << "Masukkan angka pertama: ";
-        cin >> a;
-        cout << "Masukkan angka kedua: ";
-        cin >> b;
-        hasil = a - b;
-        cout << "Hasil: " << hasil << endl;
-    } else if (pilihan == 3) {
-        cout << "Masukkan angka pertama: ";
-        cin >> a;
-        cout << "Masukkan angka kedua: ";
-        cin >> b;
-        hasil = a * b;
-        cout << "Hasil: " << hasil << endl;
-    } else if (pilihan == 4) {
-        cout << "Masukkan angka pertama: ";
-        cin >> a;
-        cout << "Masukkan angka kedua: ";
-        cin >> b;
-        if (b != 0) {
-            hasil = a / b;
+
+        if (pilihan == 1) {
+            hasil = tambah(a, b);
             cout << "Hasil: " << hasil << endl;
+        } else if (pilihan == 2) {
+            hasil = kurang(a, b);
+            cout << "Hasil: " << hasil << endl;
+        } else if (pilihan == 3) {
+            hasil = kali(a, b);
+            cout << "Hasil: " << hasil << endl;
+        } else if (pilihan == 4) {
+            hasil = bagi(a, b);
+            if (b != 0) {
+                cout << "Hasil: " << hasil << endl;
+            }
         } else {
-            cout << "Error: Tidak bisa dibagi dengan nol!" << endl;
+            cout << "Pilihan tidak valid!" << endl;
         }
-    } else {
-        cout << "Pilihan tidak valid!" << endl;
-    }
 
+        cout << "Apakah ingin menghitung lagi? (y/n): ";
+        cin >> ulang;
+    } while (ulang == 'y' || ulang == 'Y');
+
+    cout << "Terima kasih telah menggunakan kalkulator!" << endl;
     return 0;
 }
